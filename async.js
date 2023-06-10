98b762ea8f1ad276a07cbfd617ed47ba646aeecc
suryacodess <helloosuryaprakash@gmail.com>
2023-06-10 11:53:33 +0530

    async js

--- async.js
+++ async.js
@@ -1,13 +1,20 @@
 console.log("async-");
 
-console.log(1);
-console.log(2);
-const fetchJson = () => {
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((response) => console.log(response));
-};
+// console.log(1);
+// console.log(2);
+// const fetchJson = () => {
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((response) => response.json())
+//     .then((response) => console.log(response));
+// };
 
-setTimeout(fetchJson);
-console.log(3);
-console.log(4);
+// setTimeout(fetchJson);
+// console.log(3);
+// console.log(4);
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((response) => console.log(response))
+//   .catch((error) => console(error));
