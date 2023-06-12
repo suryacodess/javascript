397b1c080077293cb897b35bcc8fa3fd06161310
suryacodess <helloosuryaprakash@gmail.com>
2023-06-12 17:21:56 +0530

    ecma script 6

---
+++ ecma-script/array-de-structuring.js
@@ -0,0 +1,32 @@
+console.log("de struncturing-");
+
+// let arr = [1, 2, 3, 4, 5];
+// let x = arr[0];
+// let y = arr[1];
+// let z = arr[2];
+// let a = arr[3];
+// let b = arr[4];
+// console.log(x,y,z,a,b);
+
+// let [num1, num2, num3, num4, num5] = arr; //de structuring of array
+// console.log(num1,num2, num3, num4, num5);
+
+// let a = 10,
+//   b = 20;
+//traditional swapping method using third variable
+// let temp = a;
+// a = b;
+// b = temp;
+// console.log(a,b);
+
+// console.log(a, b);
+// [a, b] = [b, a]; //swapping using es6 feature - destructuring
+// console.log(a, b);
+
+// let name="surya", sname = "prakash";
+// console.log(name,sname);
+// [name,sname] = [sname, name];
+// console.log(name,sname);
+
+// let nums = [1, 2, 3, [4, 5, 6, [7, 8, 9]]];
+// console.log(nums.flat(2)); //hof
---
+++ ecma-script/object-de-structuring.js
@@ -0,0 +1,15 @@
+console.log("object de structuring-");
+
+// let hotel = {
+//   name: "novotel",
+//   location: "rk beach",
+//   openingsHours: {
+//     weekdays: { open: "9:00 am", close: "11:00 pm" },
+//     weekends: { open: "10:00 am", close: "12:am" },
+//   },
+// };
+// console.log(hotel);
+
+let { name } = hotel; //objects destructuring 
+// console.log(name,location,openingsHours);
+// console.log(name);
