3c894a06c23326c09a2b0f33f5b977a7b920d662
suryacodess <helloosuryaprakash@gmail.com>
2023-06-22 10:12:01 +0530

    objects

--- ecma-script/object-de-structuring.js
+++ ecma-script/object-de-structuring.js
@@ -1,15 +1,19 @@
 console.log("object de structuring-");
 
-// let hotel = {
-//   name: "novotel",
-//   location: "rk beach",
-//   openingsHours: {
-//     weekdays: { open: "9:00 am", close: "11:00 pm" },
-//     weekends: { open: "10:00 am", close: "12:am" },
-//   },
-// };
-// console.log(hotel);
+let hotel = {
+  name: "novotel",
+  location: "rk beach",
+  openingsHours: {
+    weekdays: { open: "9:00 am", close: "11:00 pm" },
+    weekends: { open: "10:00 am", close: "12:am" },
+  },
+};
+console.log(hotel);
 
-let { name } = hotel; //objects destructuring 
+// let { name } = hotel; //objects destructuring 
 // console.log(name,location,openingsHours);
 // console.log(name);
+
+
+// let {openingsHours} = hotel;
+// console.log(openingsHours);
